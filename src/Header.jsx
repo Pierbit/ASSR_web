c29504a85d12f0982e0307ddcbb0f243e88d9db9
Pierbit <pierpaolocammardella@gmail.com>
2025-05-27 11:17:01 +0200

    3.0

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,16 +4,38 @@ import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
 import './assets/components_css/header.css'
+import {useEffect, useState} from "react";
 
 function Header() {
+
+    const [flag, setFlag] = useState("Albion Small Scale Reporter");
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            if(window.innerWidth < 768){
+                setFlag("ASSR");
+            }else{
+                setFlag("Albion Small Scale Reporter");
+            }
+        };
+
+        checkScreenSize();
+
+        window.addEventListener('resize', checkScreenSize);
+
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
+
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark" className="headbar">
                 <Container>
-                    <Navbar.Brand to="/" className="main_name">Albion Small Scale Reporter</Navbar.Brand>
+                    <Navbar.Brand to="/" className="main_name">{flag}</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Daily battles</Nav.Link>
-                        <Nav.Link as={Link} to="/week">Weekly report</Nav.Link>
+                        <Nav.Link className={"header_link"} as={Link} to="/">Daily battles</Nav.Link>
+                        <Nav.Link className={"header_link"} as={Link} to="/week">Weekly report</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
